fdeae9fab2487f5dd6064a87a685f73f213b7edf
FrederikDanielsen <danielsen.contact@gmail.com>
2025-01-24 16:34:01 +0100

    Added Doxygen to constants.h

--- main/constants.h
+++ main/constants.h
@@ -3,6 +3,8 @@
 #ifndef CONSTANS_H
 #define CONSTANS_H
 
+
+/** @brief Enum that takessymbolizes the states of the Smart Security Box (mainly used in main.ino). */
 enum State {IDLE = 0, ENTERING_PASSWORD = 1, ACCESS_DENIED = 2, WRONG_PASSWORD = 3, CORRECT_PASSWORD = 4, BOX_OPEN = 5};
 
 
