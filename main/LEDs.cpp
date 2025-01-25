c27d6f4a4e6f582a138a0ed46328a0e6f96e5278
melekozel <s221706@student.dtu.dk>
2025-01-25 13:37:32 +0100

    added doxygen to LED

--- main/LEDs.cpp
+++ main/LEDs.cpp
@@ -1,9 +1,21 @@
-/** @file LEDs.cpp */
+/** 
+ * @file LEDs.cpp
+ * @brief Implementation file for the LEDs class, which controls LED behavior based on system state.
+ */
 
 #include "Arduino.h"
 #include "constants.h"
 #include "LEDs.h"
 
+/**
+ * @brief Constructor for the LEDs class.
+ * 
+ * Initializes the LED pins and sets them as outputs.
+ * 
+ * @param greenPIN The pin number connected to the green LED.
+ * @param yellowPIN The pin number connected to the yellow LED.
+ * @param redPIN The pin number connected to the red LED.
+ */
 LEDs::LEDs(int greenPIN, int yellowPIN, int redPIN) {
 
     this->greenPIN = greenPIN;
@@ -13,40 +25,47 @@ LEDs::LEDs(int greenPIN, int yellowPIN, int redPIN) {
     pinMode(this->greenPIN, OUTPUT);
     pinMode(this->yellowPIN, OUTPUT);
     pinMode(this->redPIN, OUTPUT);
-
 }
 
+/**
+ * @brief Updates the LEDs based on the provided system state.
+ * 
+ * Sets the state of the LEDs to indicate the current system state.
+ * 
+ * @param state The current state of the system, which determines the LED behavior.
+ */
 void LEDs::update(enum State state) {
 
-  switch (state) {
-            case IDLE:
-            case ENTERING_PASSWORD:
-                digitalWrite(this->yellowPIN, HIGH); // Yellow LED on
-                digitalWrite(this->greenPIN, LOW);   // Green LED off
-                digitalWrite(this->redPIN, LOW);  // Red LED off
-                break;
-            
-            case WRONG_PASSWORD:
-                digitalWrite(this->yellowPIN, LOW); // Yellow LED off
-                digitalWrite(this->greenPIN, LOW);   // Green LED off
-                digitalWrite(this->redPIN, HIGH);  // Red LED on
-            
-            case ACCESS_DENIED:
-                digitalWrite(this->yellowPIN, LOW); // Yellow LED off
-                digitalWrite(this->greenPIN, LOW);   // Green LED off
-                digitalWrite(this->redPIN, HIGH);  // Red LED on
-                break;
-            
-            case CORRECT_PASSWORD:
-                digitalWrite(this->yellowPIN, LOW);  // turn off yellow
-                digitalWrite(this->greenPIN, HIGH);  // turn on green
-                digitalWrite(this->redPIN,LOW); // turn off red
-                break;
+    switch (state) {
+        case IDLE:
+        case ENTERING_PASSWORD:
+            digitalWrite(this->yellowPIN, HIGH); // Yellow LED on
+            digitalWrite(this->greenPIN, LOW);   // Green LED off
+            digitalWrite(this->redPIN, LOW);    // Red LED off
+            break;
 
-            case BOX_OPEN:
-                digitalWrite(this->yellowPIN, LOW);  // turn off yellow
-                digitalWrite(this->greenPIN, HIGH);  // turn on green
-                digitalWrite(this->redPIN,LOW); // turn off red
-                break;
-        }
+        case WRONG_PASSWORD:
+            digitalWrite(this->yellowPIN, LOW); // Yellow LED off
+            digitalWrite(this->greenPIN, LOW);   // Green LED off
+            digitalWrite(this->redPIN, HIGH);  // Red LED on
+            break;
+
+        case ACCESS_DENIED:
+            digitalWrite(this->yellowPIN, LOW); // Yellow LED off
+            digitalWrite(this->greenPIN, LOW);   // Green LED off
+            digitalWrite(this->redPIN, HIGH);  // Red LED on
+            break;
+
+        case CORRECT_PASSWORD:
+            digitalWrite(this->yellowPIN, LOW);  // Yellow LED off
+            digitalWrite(this->greenPIN, HIGH);  // Green LED on
+            digitalWrite(this->redPIN, LOW);    // Red LED off
+            break;
+
+        case BOX_OPEN:
+            digitalWrite(this->yellowPIN, LOW);  // Yellow LED off
+            digitalWrite(this->greenPIN, HIGH);  // Green LED on
+            digitalWrite(this->redPIN, LOW);    // Red LED off
+            break;
+    }
 }
--- main/LEDs.h
+++ main/LEDs.h
@@ -1,24 +1,50 @@
-/** @file LEDs.h */
+/** 
+ * @file LEDs.h
+ * @brief Header file for the LEDs class, which controls LED pins based on the system state.
+ */
 
 #ifndef LEDS_H
 #define LEDS_H
 
 #include "constants.h"
 
-
+/**
+ * @class LEDs
+ * @brief A class to manage and update the state of LEDs based on the given state.
+ */
 class LEDs {
 
     public:
 
-        int greenPIN, yellowPIN, redPIN;
+        /**
+         * @brief The pin number for the green LED.
+         */
+        int greenPIN;
 
+        /**
+         * @brief The pin number for the yellow LED.
+         */
+        int yellowPIN;
+
+        /**
+         * @brief The pin number for the red LED.
+         */
+        int redPIN;
+
+        /**
+         * @brief Constructor for the LEDs class.
+         * @param greenPIN The pin number connected to the green LED.
+         * @param yellowPIN The pin number connected to the yellow LED.
+         * @param redPIN The pin number connected to the red LED.
+         */
         LEDs(int greenPIN, int yellowPIN, int redPIN);
 
+        /**
+         * @brief Updates the LEDs based on the provided state.
+         * @param state The current state of the system, determining the LED configuration.
+         */
         void update(enum State state);
 
 };
 
-
-
 #endif
-
